1c2e04a91e16b0dca384122e05798b58f8b44554
apw601 <apw601@gmail.com>
2018-06-08 16:51:31 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,11 +3,23 @@ class Dog
     @this_dogs_name_is_fido
   end
   
+  def dog_name
+    @this_dogs_name_is_fido
+  end
+  
   def dog_name(snoopy)
     @this_dogs_name_is_snoopy
   end
   
+   def dog_name
+    @this_dogs_name_is_snoopy
+  end
+  
   def dog_name(lassie)
+    @this_dogs_name_is_lassie
+  end
+  
+   def dog_name
     @this_dogs_name_is_lassie
   end
 end
