312379ca8cacba3a7874a75f13516369f091e2e7
apw601 <apw601@gmail.com>
2018-06-08 16:49:24 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,13 @@
-# dog.rb
+class Dog
+  def dog_name(fido)
+    @this_dogs_name_is_fido
+  end
+  
+  def dog_name(snoopy)
+    @this_dogs_name_is_snoopy
+  end
+  
+  def dog_name(lassie)
+    @this_dogs_name_is_lassie
+  end
+end
