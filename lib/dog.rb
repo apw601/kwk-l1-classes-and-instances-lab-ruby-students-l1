9cab01cbcba81b163c8d08664d2473d1592a914d
apw601 <apw601@gmail.com>
2018-06-08 16:59:30 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,24 +1,8 @@
 class Dog
-  def name=(fido)
+  def name=(dog_name)
     @this_dogs_name_is_fido
   end
-  
-  def name
-    @this_dogs_name_is_fido
-  end
-  
-  def name=(snoopy)
-    @this_dogs_name_is_snoopy
-  end
-  
-   def name
-    @this_dogs_name_is_snoopy
-  end
-  
-  def name=(lassie)
-    @this_dogs_name_is_lassie
-  end
-  
+
    def name
     @this_dogs_name_is_lassie
   end
