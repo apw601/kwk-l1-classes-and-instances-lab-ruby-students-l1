b10696b0320e36be66fbb28eadef92d897a173e3
apw601 <apw601@gmail.com>
2018-06-08 17:01:05 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,16 @@
-# person.rb
+class Person
+  def name=(person_name)
+    @this_persons_name = person_name
+  end
+
+   def name
+    @this_persons_name
+  end
+end
+
+fido = Person.new
+fido.name
+snoopy = Person.new
+snoopy.name
+lassie = Dog.new
+lassie.name
